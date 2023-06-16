142be9dcc8d2f11bc3fcd03c268eb8d0f620ef8c
Solomon594 <antirisolomon594@gmail.com>
2023-06-16 03:12:23 +0000

    web stack debug 4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,43 +1,43 @@
-# Create the "holberton" user
+# Set up the holberton user
 user { 'holberton':
-  ensure => present,
+  ensure     => present,
   managehome => true,
 }
 
-# Grant necessary permissions to the "holberton" user
-user { 'holberton':
-  groups => ['sudo'],
+# Allow SSH login for the holberton user
+ssh_authorized_key { 'holberton':
+  user  => 'holberton',
+  type  => 'ssh-rsa',
+  key   => '<holberton_public_key>',
+  ensure => present,
 }
 
-# Change the ownership and permissions of the directory where the file is located
-file { '/path/to/directory':
-  ensure => directory,
-  owner  => 'holberton',
-  group  => 'holberton',
-  mode   => '0755',
-}
-
-# Change the ownership and permissions of the file
-file { '/path/to/file':
-  ensure => file,
-  owner  => 'holberton',
-  group  => 'holberton',
-  mode   => '0644',
-}
-
-# Update the SSH configuration to allow login with the "holberton" user
+# Update SSH configuration to allow password authentication
 file { '/etc/ssh/sshd_config':
-  ensure  => present,
+  ensure  => file,
   owner   => 'root',
   group   => 'root',
   mode    => '0644',
-  content => template('your_module/sshd_config.erb'),
+  content => template('ssh/sshd_config.erb'),
   notify  => Service['ssh'],
 }
 
-# Restart the SSH service
+# Create custom SSH configuration template
+file { '/etc/ssh/sshd_config.erb':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "<%-|
+    # This file is managed by Puppet
+    PasswordAuthentication yes
+  |-%>\n",
+  notify  => Service['ssh'],
+}
+
+# Restart SSH service
 service { 'ssh':
-  ensure => running,
-  enable => true,
+  ensure  => running,
+  enable  => true,
   require => File['/etc/ssh/sshd_config'],
 }
